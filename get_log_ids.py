b1453969aa3f2a89155a9db3a3a0692db8b768ce
Astatham98 <alex.statham1998@gmail.com>
2023-10-27 15:55:57 +0100

    database gathering complete
    logic for the main season info gathering is sorted. Adds to a database with a table for each class currently. Still needs some tweaking with logs and adding logging.

--- get_log_ids.py
+++ get_log_ids.py
@@ -4,21 +4,26 @@ import pickle
 def get_etf2l_stats(match):
     
     url = f"https://api-v2.etf2l.org/matches/{match}"
-    web = requests.get(url).json()
-    match_stats = web['match']
-    if match_stats['r1'] is not None:
-        player_team_ids = {player['steam']['id64']: player['team_id'] for player in match_stats['players']}
-        
-        c1id = match_stats['clan1']['id']
-        c2id = match_stats['clan2']['id']
-        
-        c1score = match_stats['r1']
-        c2score = match_stats['r2']
-        
-        player_score_ids_c1 = {id: c1score for id, tid in player_team_ids.items() if tid == c1id}
-        player_score_ids_c2 = {id: c2score for id, tid in player_team_ids.items() if tid == c2id}
-        merged_score_ids = player_score_ids_c1 | player_score_ids_c2
-        return merged_score_ids, match_stats['time'], match_stats['submitted'], match_stats['maps']
+    web = requests.get(url)
+    if web.status_code == 200:
+        web_json = web.json()
+        match_stats = web_json['match']
+        if match_stats['r1'] is not None:
+            player_team_ids = {player['steam']['id64']: player['team_id'] for player in match_stats['players']}
+            
+            c1id = match_stats['clan1']['id']
+            c2id = match_stats['clan2']['id']
+            
+            c1score = match_stats['r1']
+            c2score = match_stats['r2']
+            
+            player_score_ids_c1 = {id: c1score for id, tid in player_team_ids.items() if tid == c1id}
+            player_score_ids_c2 = {id: c2score for id, tid in player_team_ids.items() if tid == c2id}
+            id64_week = {id: match_stats['round'] for id in player_team_ids.keys()}
+            merged_score_ids = player_score_ids_c1 | player_score_ids_c2
+            return merged_score_ids, match_stats['time'], match_stats['submitted'], match_stats['maps'], id64_week
+    else:
+        print(url, ' ', web.status_code)
     return
         
 def get_logs_stats(id64, before, after, etf2l_maps):
@@ -64,11 +69,14 @@ def get_log_ids(pickeList):
     with open(pickeList, "rb") as fp:
             match_ids = pickle.load(fp)
     ids = []
+    id64_scores = {}
+    id64_week = {}
     for match in match_ids:
         if get_etf2l_stats(match) is not None:
-            id64_score, game_time, submitted_time, maps = get_etf2l_stats(match)
+            id64_score, game_time, submitted_time, maps, week = get_etf2l_stats(match)
             logs = get_logs_stats(id64_score.keys(), game_time, submitted_time, maps)
             ids.append([log['id'] for log in logs])
+            id64_scores = id64_scores | id64_score
+            id64_week = id64_week | week
         else: continue
-    
-    return ids, id64_score
+    return ids, id64_scores, id64_week
--- parse_logs.py
+++ parse_logs.py
@@ -1,9 +1,10 @@
 import requests
 from get_log_ids import get_log_ids
 from steamid_converter import Converter
+import sqlite3
 
 def log_parser(pickleList):
-    ids, id64_score = get_log_ids(pickleList)
+    ids, id64_score, id64_week = get_log_ids(pickleList)
     for match in ids:
         for log in match:
             log_json = requests.get('http://logs.tf/json/{}'.format(log)).json()
@@ -11,18 +12,39 @@ def log_parser(pickleList):
                 return
             else:
                 players = log_json['players']
+                time = log_json['length']
                 for player_id, player_values in players.items():
                     player_id64 = Converter.to_steamID64(player_id)
                     mainClass = getMainClass(player_values)
+                    score = id64_score.get(player_id64)
+                    week = id64_week.get(player_id64)
+                    print(week)
                     match mainClass:
                         case 'scout':
-                            get_scout_stats(player_values, id64_score.get(player_id64))
+                            scout_stats = get_scout_stats(player_values, time)
+
+                            put_scout_db(scout_stats, score, player_id64, week)
                         case 'soldier':
-                            get_soldier_stats(player_values, id64_score.get(player_id64))
+                            soldier_stats = get_soldier_stats(player_values, time)
+
+                            put_soldier_db(soldier_stats, score, player_id64, week)
                         case 'demoman':
-                            get_demoman_stats(player_values, id64_score.get(player_id64))
+                            demoman_stats = get_demoman_stats(player_values, time)
+
+                            put_demoman_db(demoman_stats, score, player_id64, week)
                         case 'medic':
-                            get_medic_stats(player_values, id64_score.get(player_id64))
+                            med_stats = get_medic_stats(player_values,  time)
+                            
+                            put_medic_db(med_stats, score, player_id64, week)
+
+    con = sqlite3.connect("s45")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM MEDIC")
+    res = cur.fetchall()
+    for res in res:
+        print(res)
+    con.close()
+                            
                     
                  
 def getMainClass(player):
@@ -35,13 +57,98 @@ def getMainClass(player):
             best_time = stats['total_time']
     return mainClass
 
-def get_medic_stats(player, score):
-    pass
-def get_demoman_stats(player, score):
-    pass
-def get_soldier_stats(player, score):
-    pass
-def get_scout_stats(player, score):
-    pass
+def get_medic_stats(player, time):
+    kills = player['kills']
+    deaths = player['deaths']
+    assists = player['assists']
+    kapd = player['kapd']
+    ubers = player['ubers']
+    drops = player['drops']
+    heals = player['heal']  
+    
+    heals_per_min = int(heals / (time/60))
+    airshots = player['as']
+    return [kills, deaths, assists, kapd, ubers, drops, heals_per_min, airshots]
+def get_demoman_stats(player, time):
+    kills = player['kills']
+    deaths = player['deaths']
+    assists = player['assists']
+    kapd = player['kapd']
+    airshots = player['as']
+    dmg = player['dmg_real']
+    dpm = int(dmg / (time/60))
+    
+    return [kills, deaths, assists, kapd, airshots, dpm]
+def get_soldier_stats(player, time):
+    kills = player['kills']
+    deaths = player['deaths']
+    assists = player['assists']
+    kapd = player['kapd']
+    airshots = player['as']
+    dmg = player['dmg_real']
+    dpm = int(dmg / (time/60))
+    
+    return [kills, deaths, assists, kapd, airshots, dpm]
+def get_scout_stats(player, time):
+    kills = player['kills']
+    deaths = player['deaths']
+    assists = player['assists']
+    kapd = player['kapd']
+    dmg = player['dmg_real']
+    dpm = int(dmg / (time/60))
+    
+    return [kills, deaths, assists, kapd, dpm]
 
-log_parser('prem_match_ids')
+def put_medic_db(stats, score, id, week):
+    idweek = id + "_" + week
+    to_insert = [idweek] + [id] + stats + [score]
+    
+    con = sqlite3.connect("s45")
+    cur = con.cursor()
+    
+    cur.execute(
+    """CREATE TABLE if not exists MEDIC (IDWEEK VARCHAR(255), ID INT, KILLS int, DEATHS INT, ASSISTS INT, KAPD VARCHAR(255), UBERS INT, DROPS INT, HPM INT, AIRSHOTS INT, SCORE INT);""")
+    cur.execute("INSERT INTO MEDIC VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", to_insert)
+    con.commit()
+    con.close()
+    
+def put_demoman_db(stats, score, id, week):
+    idweek = id + "_" + week
+    to_insert = [idweek] + [id] + stats + [score]
+    
+    con = sqlite3.connect("s45")
+    cur = con.cursor()
+    
+    cur.execute(
+        """CREATE TABLE if not exists DEMOMAN (IDWEEK VARCHAR(255), ID INT, KILLS int, DEATHS INT, ASSISTS INT, KAPD VARCHAR(255), AIRSHOTS INT, DPM INT, SCORE INT);""")
+    cur.execute("INSERT INTO DEMOMAN VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", to_insert)
+    con.commit()
+    con.close()
+    
+def put_soldier_db(stats, score, id, week):
+    idweek = id + "_" + week
+    to_insert = [idweek] + [id] + stats + [score]
+    
+    con = sqlite3.connect("s45")
+    cur = con.cursor()
+    
+    cur.execute(
+        """CREATE TABLE if not exists SOLDIER (IDWEEK VARCHAR(255), ID INT, KILLS int, DEATHS INT, ASSISTS INT, KAPD VARCHAR(255), AIRSHOTS INT, DPM INT, SCORE INT);""")
+    cur.execute("INSERT INTO SOLDIER VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", to_insert)
+    con.commit()
+    con.close()
+    
+def put_scout_db(stats, score, id, week):
+    idweek = str(id) + "_" + week
+    to_insert = [idweek] + [id] + stats + [score]
+    
+    con = sqlite3.connect("s45")
+    cur = con.cursor()
+    
+    cur.execute(
+        """CREATE TABLE if not exists SCOUT (IDWEEK VARCHAR(255), ID INT, KILLS int, DEATHS INT, ASSISTS INT, KAPD VARCHAR(255), DPM INT, SCORE INT);""")
+    cur.execute("INSERT INTO SCOUT VALUES(?, ?, ?, ?, ?, ?, ?, ?)", to_insert)
+    con.commit()
+    con.close()
+    
+log_parser('prem_match_ids_prev')
